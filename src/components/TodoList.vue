50a9ae24a54f1b6de95f3e8351d0da1f238127ed
Shaher Yar Munir <140485614+Shaher-Yar@users.noreply.github.com>
2023-11-30 16:50:44 +0500

    Initial commit

---
+++ src/components/TodoList.vue
@@ -0,0 +1,40 @@
+<template>
+  <div>
+    <h1>Todo App</h1>
+    <input v-model="newTask" @keyup.enter="addTask" placeholder="Add a new task" />
+    <ul>
+      <li v-for="(task, index) in tasks" :key="index">
+        {{ task }}
+        <button @click="removeTask(index)">Remove</button>
+      </li>
+    </ul>
+  </div>
+</template>
+
+<script>
+export default {
+  data() {
+    return {
+      newTask: '',
+      tasks: []
+    };
+  },
+  methods: {
+    addTask() {
+      if (this.newTask.trim() !== '') {
+        this.tasks.push(this.newTask);
+        this.newTask = '';
+      }
+    },
+    removeTask(index) {
+      this.tasks.splice(index, 1);
+    }
+  }
+};
+
+</script>
+  
+
+  <style scoped>
+/* Add component-specific styles here if needed */
+</style>
